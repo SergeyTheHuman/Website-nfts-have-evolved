827af93500bcfcb009d0c20257d3d808754c1bfe
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-05-18 13:03:14 +0300

    added partners section + vibration button

--- js/app.js
+++ js/app.js
@@ -3,8 +3,13 @@ import { isWebp } from './components/isWebp.js'
 isWebp()
 
 const $burgerButton = document.querySelector('.menu__burger-button')
+const $joinButton = document.querySelector('.header__join')
 
 $burgerButton.addEventListener('click', () => {
 	document.body.classList.toggle('active')
 	$burgerButton.classList.toggle('active')
+})
+$joinButton.addEventListener('click', () => {
+	console.log('Вибрация!')
+	window.navigator.vibrate([400, 200, 400])
 })
